cd249153ccd164a8e0363f6128808c663eb08f0a
CrazyBaby6666 <Tomer90902@gmail.com>
2026-05-18 19:04:33 +0300

    pizdetz

--- HW/Mediator/ControlTower.cs
+++ HW/Mediator/ControlTower.cs
@@ -2,5 +2,11 @@
 
 public class ControlTower : IMediator;
 {
-    
-}
+    private queue<Airplane> _airplanesQueue;
+    private queue<Helicopter> _chooper;
+
+    public ControlTower(Helicopter _chooper = null, Airplane _airplane = null)
+    {
+        new Queue<Airplane>();
+        new Queue<Helicopter>();
+    }
--- HW/Mediator/IMediator.cs
+++ HW/Mediator/IMediator.cs
@@ -2,5 +2,5 @@
 
 public interface IMediator
 {
-    
+    void Notify (Airplane sender, string message);;
 }
