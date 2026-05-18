331e621e06f6ab3b6e5de61e0decb187eb9108f1
CrazyBaby6666 <Tomer90902@gmail.com>
2026-05-18 19:08:30 +0300

    PushaT

--- HW/Mediator/ControlTower.cs
+++ HW/Mediator/ControlTower.cs
@@ -10,3 +10,15 @@ public class ControlTower : IMediator;
         new Queue<Airplane>();
         new Queue<Helicopter>();
     }
+
+    public void Notify(AirVehicle sender, string message)
+    {
+        if (message == "TakeOff")
+        {
+            Console.WriteLine("Control Tower: " + sender.GetType().Name + " is taking off.");
+        }
+        if(message == "Land")
+        {
+            Console.WriteLine("Control Tower: " + sender.GetType().Name + " is landing.");
+        }
+    }
